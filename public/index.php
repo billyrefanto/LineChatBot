1b69a49f46259f163514f9706189d33cd305fcf8
Moch Billy Refanto <mochbillyrefanto@gmail.com>
2020-04-04 13:55:01 +0700

    Implementasi Balasan

--- public/index.php
+++ public/index.php
@@ -10,20 +10,25 @@ use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
 use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use \LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\AudioMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use \LINE\LINEBot\MessageBuilder\VideoMessageBuilder;
 use \LINE\LINEBot\SignatureValidator as SignatureValidator;
  
 $pass_signature = true;
  
 // set LINE channel_access_token and channel_secret
-$channel_access_token = "";
-$channel_secret = "";
+$channel_access_token = "pSZbmpoqs27R1HVF/IbFtdYIhrOLrnBudlT6N+p4zXI0hGOyqJTjgb9m0F1NOs3iNQErlrF8yVyzdUzlook8LtLaMKAIZ7Ee5DtpQXS0fQtJsmFK/TE2PR5jCz6cJNuxOWpN9NC0MJPUTMLk3b+DSgdB04t89/1O/w1cDnyilFU=";
+$channel_secret = "856fb55c1ec736082f03fcfb65f83b94";
  
 // inisiasi objek bot
 $httpClient = new CurlHTTPClient($channel_access_token);
 $bot = new LINEBot($httpClient, ['channelSecret' => $channel_secret]);
  
+ 
 $app = AppFactory::create();
 $app->setBasePath("/public");
+ 
  
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello World!");
@@ -50,8 +55,32 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
             return $response->withStatus(400, 'Invalid signature');
         }
     }
-    
-// kode aplikasi nanti disini
+ 
+    $data = json_decode($body, true);
+    if(is_array($data['events'])){
+        foreach ($data['events'] as $event)
+        {
+            if ($event['type'] == 'message')
+            {
+                if($event['message']['type'] == 'text')
+                {
+                    // send same message as reply to user
+                    $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+ 
+ 
+                    // or we can use replyMessage() instead to send reply message
+                    // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                    // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+ 
+ 
+                    $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus($result->getHTTPStatus());
+                }
+            }
+        }
+    }
  
 });
 $app->run();
