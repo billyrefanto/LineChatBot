dda7afaf63380b15f2814609b2794dbcdf83e9e1
Moch Billy Refanto <mochbillyrefanto@gmail.com>
2020-04-08 01:40:19 +0700

    implementasi Flex Message

--- public/index.php
+++ public/index.php
@@ -169,4 +169,61 @@ $app->get('/content/{messageId}', function ($req, $response, $args) use ($bot) {
         ->withHeader('Content-Type', $result->getHeader('Content-Type'))
         ->withStatus($result->getHTTPStatus());
 });
+
+// flex
+$app->post('/webhook', function (Request $request, Response $response) use ($channel_secret, $bot, $httpClient, $pass_signature) {
+ 
+    ...
+ 
+    $data = json_decode($body, true);
+    if (is_array($data['events'])) {
+        foreach ($data['events'] as $event) {
+            if ($event['type'] == 'message') {
+                // message from group / room
+                if ($event['source']['type'] == 'group' or
+                    $event['source']['type'] == 'room'
+                ) {
+ 
+                    ...
+ 
+                // message from single user
+                } else {
+                    if ($event['message']['type'] == 'text') {
+                    if (strtolower($event['message']['text']) == 'user id') {
+ 
+                        $result = $bot->replyText($event['replyToken'], $event['source']['userId']);
+ 
+                    } elseif (strtolower($event['message']['text']) == 'flex message') {
+ 
+                        $flexTemplate = file_get_contents("../flex_message.json"); // template flex message
+                        $result = $httpClient->post(LINEBot::DEFAULT_ENDPOINT_BASE . '/v2/bot/message/reply', [
+                            'replyToken' => $event['replyToken'],
+                            'messages'   => [
+                                [
+                                    'type'     => 'flex',
+                                    'altText'  => 'Test Flex Message',
+                                    'contents' => json_decode($flexTemplate)
+                                ]
+                            ],
+                        ]);
+ 
+                    } else {
+                        // send same message as reply to user
+                        $result = $bot->replyText($event['replyToken'], $event['message']['text']);
+                    }
+ 
+                    $response->getBody()->write($result->getJSONDecodedBody());
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus($result->getHTTPStatus());
+                }
+                    ...
+ 
+                }
+            }
+        }
+    }
+ 
+    return $response->withStatus(400, 'No event sent!');
+});
 $app->run();
